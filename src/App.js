96bcd940f512578d0c97c3228ff89c878031a7e0
Tamires Cavalcante <tmires.cavalcante@gmail.com>
2022-01-26 15:30:39 -0300

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
 
-function App() {
+function App () {
+
+  const [movieList, setmovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
   useEffect(() => {
     const loadAll = async () => {
       //Pegando a lista total
       let list = await Tmdb.getHomeList();
-      console.log(list);
+      setmovieList(list);
+
+      //Pegando o featured
+      let originals = list.filter(i => i.slug === 'originals')
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+
+      setFeaturedData(chosenInfo);
     }
 
     loadAll();
   }, []);
 
   return (
-    <div className="App">
-      
+    <div className="page">
+
+      <Header />
+
+      {featuredData && 
+        <FeaturedMovie item={featuredData} />
+      }
+
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <div>
+            <MovieRow key={key} title={item.title} items={item.items} />
+          </div>
+        ))}
+      </section>
+      {/* Header
+      Destaques
+      Listas
+      Rodapé */}
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Header.css';
+import Netflix from '../components/img/netflix.png';
+import User from '../components/img/user.png'
+
+function Header({black}) {
+  return (
+      <header className={black ? "black" : ""}>
+        <div className="header--logo">
+            <a href="/">
+                <img src={Netflix} alt="NetFlix" />
+            </a>
+        </div>
+        <div className="header--user">
+            <a href="/">
+                <img src={User} alt="usuario"/>
+            </a>
+        </div>
+      </header>
+  );
+}
+
+export default Header;
